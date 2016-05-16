32b2457f618b65512ea67dcda0025d2c9bccc869
Xavier Duncan <xduncan@tuta.io>
2016-05-16 14:57:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -82,4 +82,14 @@ app.controller('PostsCtrl', [
     // using the id from $stateParams
     $scope.post = posts.posts[$stateParams.id];
 
+    $scope.addComment = function() {
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+
 }]);
